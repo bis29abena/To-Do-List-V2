1771b723bedb495f7a755fbe859a177e16c6d806
Bismark Osei <bismarkoseiabena@gmail.com>
2022-06-16 21:12:34 +0000

    Initial commit

---
+++ src/components/InputArea.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function InputArea(props) {
+  return (
+    <div className="form">
+      <input
+        onChange={props.onHandleChange}
+        type="text"
+        value={props.addValue}
+      />
+      <button onClick={props.onAddItem}>
+        <span>Add</span>
+      </button>
+    </div>
+  );
+}
+
+export default InputArea;
